5f9153d46096af6aa380251022113aa0f5879b39
AmanSalman <rapamansalman@gmail.com>
2024-07-04 22:46:28 +0300

    add payment order

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -1,124 +1,151 @@
+import { OrderModel } from "../../../DB/models/order.model.js";
+import { ProductModel } from "../../../DB/models/product.model.js";
+import { CartModel } from "./../../../DB/models/cart.model.js";
+import { UserModel } from "./../../../DB/models/user.model.js";
+import { CouponModel } from "./../../../DB/models/Coupon.model.js";
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.SecretKey);
 
-import { OrderModel } from '../../../DB/models/order.model.js';
-import { ProductModel } from '../../../DB/models/product.model.js';
-import { CartModel } from './../../../DB/models/cart.model.js';
-import { UserModel } from './../../../DB/models/user.model.js';
-import { CouponModel } from './../../../DB/models/Coupon.model.js';
-
-export const create = async (req,res) =>{
-  const cart = await CartModel.find({userId:req.user.id})
-  if(!cart|| cart.products.length === 0){
-    return res.status(400).json({message:'cart is empty'})
+export const create = async (req, res) => {
+  const cart = await CartModel.find({ userId: req.user.id });
+  if (!cart || cart?.products?.length === 0) {
+    return res.status(400).json({ message: "cart is empty" });
   }
 
-  req.body.products= cart[0].products
+  req.body.products = cart[0].products;
 
-  if(req.body.couponId){
-    const coupon = await CouponModel.findById(req.body.couponId)
-    if(!coupon){
-      return res.status(400).json({message:'coupon not found'})
+  if (req.body.couponId) {
+    const coupon = await CouponModel.findById(req.body.couponId);
+    if (!coupon) {
+      return res.status(400).json({ message: "coupon not found" });
     }
-    if(coupon.expireDate < Date.now()){
-      return res.status(400).json({message:'coupon is expired'})
+    if (coupon.expireDate < Date.now()) {
+      return res.status(400).json({ message: "coupon is expired" });
     }
-    if(coupon.usedBy.includes(req.user.id)){
-      return res.status(400).json({message:'coupon is already used'})
+    if (coupon.usedBy.includes(req.user.id)) {
+      return res.status(400).json({ message: "coupon is already used" });
     }
-    req.body.coupon = coupon
+    req.body.coupon = coupon;
   }
 
-  let finalProductList = []
-  let subtotal = 0
-  for(let product of req.body.products){
+  let finalProductList = [];
+  let subtotal = 0;
+  for (let product of req.body.products) {
     const checkproduct = await ProductModel.findOne({
-      _id:product.productId,
-      stock:{$gte:product.quantity}
-    })
-    if(!checkproduct){
-      return res.status(400).json({message:'product quantity not available'})
+      _id: product.productId,
+      stock: { $gte: product.quantity },
+    });
+    if (!checkproduct) {
+      return res
+        .status(400)
+        .json({ message: "product quantity not available" });
     }
 
-    product = product.toObject()
-    product.name = checkproduct.name
-    product.discount = checkproduct.discount
-    product.unitPrice = checkproduct.price
-    product.finalPrice = checkproduct.FinalPrice
-    subtotal+= product.finalPrice
-    finalProductList.push(product)
+    product = product.toObject();
+    product.name = checkproduct.name;
+    product.discount = checkproduct.discount;
+    product.unitPrice = checkproduct.price;
+    product.finalPrice = checkproduct.FinalPrice;
+    subtotal += product.finalPrice;
+    finalProductList.push(product);
   }
 
-  const user = await UserModel.findById(req.user.id)
-  if(!req.body.Address){
-    req.body.Address = user.address
+  const user = await UserModel.findById(req.user.id);
+  if (!req.body.Address) {
+    req.body.Address = user.address;
   }
-  if(!req.body.phone){
-    req.body.phone = user.phone
+  if (!req.body.phone) {
+    req.body.phone = user.phone;
   }
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: "USD",
+          unit_amount:
+            subtotal - subtotal * ((req.body.coupon?.Amount || 0) / 100),
+          product_data: {
+            name: user.username,
+          },
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: `${process.env.Front_URL}`,
+    cancel_url: `${process.env.Front_URL}/cart`,
+  });
+
+  return res.json(session);
 
   const order = await OrderModel.create({
     userId: req.user.id,
     products: finalProductList,
-    finalPrice: subtotal - (subtotal * ((req.body.coupon?.Amount || 0 )/100)),
+    finalPrice: subtotal - subtotal * ((req.body.coupon?.Amount || 0) / 100),
     address: req.body.Address,
     phone: req.body.phone,
-  })
+  });
 
-    
-  if(order){
-    for(const product of req.body.products){
+  if (order) {
+    for (const product of req.body.products) {
       const updateproduct = await ProductModel.findOneAndUpdate(
         { _id: product.productId },
         { $inc: { stock: -product.quantity } }
-      )
-      
+      );
     }
 
     if (req.body.coupon) {
-      await CouponModel.findByIdAndUpdate({_id:req.body.coupon._id}, {
-        $addToSet:{
-          usedBy:req.user.id
+      await CouponModel.findByIdAndUpdate(
+        { _id: req.body.coupon._id },
+        {
+          $addToSet: {
+            usedBy: req.user.id,
+          },
         }
-      })
+      );
     }
 
-    await CartModel.findOneAndUpdate({userId:req.user.id}, {
-        products:[]
-    }) 
+    await CartModel.findOneAndUpdate(
+      { userId: req.user.id },
+      {
+        products: [],
+      }
+    );
   }
 
-  return res.status(200).json({message:"success", order})
-}
-
+  return res.status(200).json({ message: "success", order });
+};
 
 export const getorders = async (req, res) => {
-  const orders = await OrderModel.find({userId:req.user.id}).populate('userId')
-  return res.status(200).json({message:"success", orders})
-}
-
+  const orders = await OrderModel.find({ userId: req.user.id }).populate(
+    "userId"
+  );
+  return res.status(200).json({ message: "success", orders });
+};
 
 export const orders = async (req, res) => {
   const orders = await OrderModel.find({
-    $or:[
+    $or: [
       {
-        status:'pending'
+        status: "pending",
       },
       {
-        status:'confirmed'
-      }
-    ]
-  })
-  return res.status(200).json({message:"success", orders})
-}
+        status: "confirmed",
+      },
+    ],
+  });
+  return res.status(200).json({ message: "success", orders });
+};
 
-
-export const changesStatus = async (req,res) =>{
-  const {status} = req.body
-  const {id} = req.params
-  const order = await OrderModel.findById(id)
-  if(!order){
-    return res.status(404).json({message:"order not found"})
+export const changesStatus = async (req, res) => {
+  const { status } = req.body;
+  const { id } = req.params;
+  const order = await OrderModel.findById(id);
+  if (!order) {
+    return res.status(404).json({ message: "order not found" });
   }
-  order.status = status
-  await order.save()
-  return res.json(order)
-}
+  order.status = status;
+  await order.save();
+  return res.json(order);
+};
--- src/modules/order/order.validation.js
+++ src/modules/order/order.validation.js
@@ -8,7 +8,7 @@ export const IdSchema = joi.object({
 
 
 export const OrderSchema = joi.object({
-  couponId:joi.string().hex().length(24).required(),
+  couponId:joi.string().hex().length(24),
   Address: joi.string().optional(),
   phone: joi.string().optional().pattern(/^(?:(?:\+970|0)(?:2|4|8|9)\d{7}|(?:\+970|0)5(?:6|9)\d{7})$|^(?:(?:\+972|0)(?:2|3|4|8|9)\d{7}|(?:\+972|0)5\d{7})$/)
 })
