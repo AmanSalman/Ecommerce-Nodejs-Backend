00e7772f3102ac0edf469b4fea191620a1a449bb
AmanSalman <rapamansalman@gmail.com>
2024-07-05 06:02:47 +0300

    fix change QTY

--- src/modules/cart/cart.router.js
+++ src/modules/cart/cart.router.js
@@ -3,7 +3,7 @@ import * as cartController from './cart.controller.js';
 import {auth} from '../../middleware/auth.js'
 import { endpoints } from "./cart.role.js";
 import { validation } from './../../middleware/validation.js';
-import { CRCart } from "./cart.validation.js";
+import { CRCart, QTyCart } from "./cart.validation.js";
 const router = Router()
 
 router.post('/', auth(endpoints.create),cartController.create)
@@ -12,6 +12,6 @@ router.get('/', auth(endpoints.create),cartController.getCart)
 router.put('/', auth(endpoints.create),cartController.clearcart)
 router.post('/increaseQty/:productId', validation(CRCart), auth(endpoints.create), cartController.increasecart)
 router.post('/decreaseQty/:productId', validation(CRCart), auth(endpoints.create), cartController.decreasecart)
-router.post('/UpdateQty/:productId', validation(CRCart), auth(endpoints.create), cartController.updateQty)
+router.post('/UpdateQty/:productId', validation(QTyCart), auth(endpoints.create), cartController.updateQty)
 
 export default router;
--- src/modules/cart/cart.validation.js
+++ src/modules/cart/cart.validation.js
@@ -3,3 +3,9 @@ import joi from 'joi'
 export const CRCart = joi.object({
   id:joi.string().hex().length(24).required(),
 })
+
+export const QTyCart = joi.object({
+  id:joi.string().hex().length(24).required(),
+  quantity:joi.number().required(),
+  operator:joi.string().required(),
+})
