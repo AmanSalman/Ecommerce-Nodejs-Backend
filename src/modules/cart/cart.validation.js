82bdbb02eea73fd9edc97153744b47e356f94a26
AmanSalman <rapamansalman@gmail.com>
2024-07-05 06:16:03 +0300

    fix productId

--- src/modules/cart/cart.validation.js
+++ src/modules/cart/cart.validation.js
@@ -1,11 +1,11 @@
 import joi from 'joi'
 
 export const CRCart = joi.object({
-  id:joi.string().hex().length(24).required(),
+  productId:joi.string().hex().length(24).required(),
 })
 
 export const QTyCart = joi.object({
-  id:joi.string().hex().length(24).required(),
+  productId:joi.string().hex().length(24).required(),
   quantity:joi.number().required(),
   operator:joi.string().required(),
 })
