7dbf9d2e1224259c7b574416d2ef4bbeef1cf6a2
AmanSalman <82624209+AmanSalman@users.noreply.github.com>
2024-07-07 00:13:06 +0300

    Update review.validation.js

--- src/modules/review/review.validation.js
+++ src/modules/review/review.validation.js
@@ -3,5 +3,6 @@ import joi from 'joi';
 
 export const createReview = joi.object({
   comment:joi.string().required(),
-  rating:joi.number().min(1).max(5).required()
+  rating:joi.number().min(1).max(5).required(),
+  productId:joi.string().hex().length(24).required()
 })
